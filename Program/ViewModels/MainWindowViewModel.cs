d99b004315b217425154be9c0598454a68deabd4
GigelSuflaTare <92532606+GigelSuflaTare@users.noreply.github.com>
2025-11-28 18:59:20 +0200

    Avalonia Template

---
+++ Program/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Program.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Program/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Program.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
